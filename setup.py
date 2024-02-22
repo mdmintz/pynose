1a65df0a19643812934027e5a5df362ee201ca60
Michael Mintz <mdmintz@gmail.com>
2024-02-22 12:02:57 -0500

    Drop support for Python 3.6

--- setup.py
+++ setup.py
@@ -99,7 +99,7 @@ setup(
         "Documentation": "https://nose.readthedocs.io/en/latest/",
     },
     package_data={"": ["*.txt"]},
-    python_requires=">=3.6",
+    python_requires=">=3.7",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
@@ -111,7 +111,6 @@ setup(
         "Operating System :: OS Independent",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
